4a7ff426054e415acb73601a1f15d7ac1ef8b80c
dmilakov <dmilakov@eso.org>
2019-01-21 15:58:37 +0100

    v0.5.7
    Fixed bug in line LSF fitting

--- lines.py
+++ lines.py
@@ -382,7 +382,8 @@ def model(spec,fittype,line_model=None,lsf=None,nobackground=False):
             model2d[order,pixl:pixr] = line.evaluate(pars,pix)
         elif fittype == 'lsf':
             pix = np.arange(pixl,pixr)
-            lsf1d = lsf[order,segm]
+            center = linelist[i]['lsf'][1]
+            lsf1d = lsf.interpolate(order,center)
             model2d[order,pixl:pixr] = hfit.lsf_model(lsf1d,pars,pix)
     if nobackground==False:
         
