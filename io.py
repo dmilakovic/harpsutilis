8be6fd37422d962e1aad2e4c5a43ff83eebd28d9
dmilakov <dmilakov@eso.org>
2019-03-06 12:00:01 +0100

    v_1.0.2
    comb frequencies hard coded

--- io.py
+++ io.py
@@ -101,6 +101,7 @@ def read_LFC_keywords(filepath,LFC_name,anchor_offset=0):
         pixPerLine   = 22
         # wiener filter window scale
         window       = 3
+        f0_comb      = 5.7e9
     elif LFC_name=='FOCES':
         modefilter   = 100
         f0_source    = 20e6 #Hz
@@ -110,14 +111,15 @@ def read_LFC_keywords(filepath,LFC_name,anchor_offset=0):
         pixPerLine   = 35
         # wiener filter window scale
         window       = 5
+        f0_comb      = 9.27e9
     #include anchor offset if provided
-    anchor = anchor# + anchor_offset
+    #anchor = anchor# + anchor_offset
     
-    m,k            = divmod(
-                        round((anchor-f0_source)/fr_source),
-                               modefilter)
+    #m,k            = divmod(
+    #                    round((anchor-f0_source)/fr_source),
+    #                           modefilter)
     #f0_comb   = (k-1)*fr_source + f0_source + anchor_offset
-    f0_comb = k*fr_source + f0_source + anchor_offset
+    #f0_comb = k*fr_source + f0_source + anchor_offset
     LFC_keys = dict(name=LFC_name, comb_anchor=f0_comb, window_size=window,
                     source_anchor=anchor, source_reprate=source_reprate, 
                     modefilter=modefilter, comb_reprate=reprate,ppl=pixPerLine,
